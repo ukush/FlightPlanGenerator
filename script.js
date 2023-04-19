0e280ef9a210abb08563512b90dd087c3777ca29
ukush <ukushi01@gmail.com>
2023-04-19 15:58:26 +0100

    test

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let flightPath = []
 
 // --------- Init Map Settings -----------------------------//
 
-var map = L.map('map').setView([51.505, -0.09], 3);
+var map = L.map('map').setView([0,0], 2);
 
 // Add tile layer to map object
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
